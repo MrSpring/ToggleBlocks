0423a0bba5245ec4665b7f4731e76cda8320c52b
MrSpring <mrspring@mrspring.dk>
2015-06-01 16:24:52 +0200

    0.0.8-01 final

--- src/main/java/dk/mrspring/toggle/ModInfo.java
+++ src/main/java/dk/mrspring/toggle/ModInfo.java
@@ -7,5 +7,5 @@ public class ModInfo
 {
     public static final String MOD_ID = "tb";
     public static final String NAME = "Toggle Blocks";
-    public static final String VERSION = "0.0.7-02";
+    public static final String VERSION = "0.0.8-01";
 }
