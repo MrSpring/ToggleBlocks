cde1a82a261d0e48bbf8c1e331e3a5f4abd3f4ff
MrSpring <mrspring@mrspring.dk>
2015-08-26 17:41:08 +0200

    Getting from metadata, for backwards comp.

--- src/main/java/dk/mrspring/toggle/tileentity/TileEntityToggleBlock.java
+++ src/main/java/dk/mrspring/toggle/tileentity/TileEntityToggleBlock.java
@@ -642,6 +642,11 @@ public class TileEntityToggleBlock extends TileEntity implements ISidedInventory
         this.currentMode = Mode.valueOf(compound.getString(MODE));
         this.priority = StoragePriority.fromInt(compound.getInteger(PRIORITY));
         this.maxChangeBlocks = compound.getInteger(MAX_CHANGE_BLOCKS);
+        if (maxChangeBlocks == 0)
+        {
+            BlockToggleController.ControllerSize size = BlockToggleController.sizes[getBlockMetadata()];
+            maxChangeBlocks = size.size;
+        }
 
         NBTTagList storageList = compound.getTagList(ITEMS, 10);
         this.itemStacks = new ItemStack[getStorageSlots()];
