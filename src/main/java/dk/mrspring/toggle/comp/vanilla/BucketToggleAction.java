45f00d968d75e1624c56bb79ab74d5e68aa9ff2b
MrSpring <mrspring@mrspring.dk>
2015-09-29 16:09:18 +0200

    Added proper Fluid support

--- src/main/java/dk/mrspring/toggle/comp/vanilla/BucketToggleAction.java
+++ src/main/java/dk/mrspring/toggle/comp/vanilla/BucketToggleAction.java
@@ -1,8 +1,10 @@
 package dk.mrspring.toggle.comp.vanilla;
 
 import dk.mrspring.toggle.api.IToggleController;
+import dk.mrspring.toggle.api.IToggleStorage;
 import dk.mrspring.toggle.tileentity.BasicBlockToggleAction;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockStaticLiquid;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -10,6 +12,10 @@ import net.minecraft.item.ItemBucket;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidContainerRegistry;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.IFluidBlock;
 
 /**
  * Created by MrSpring on 02-03-2015 for ToggleBlocks.
@@ -19,94 +25,86 @@ public class BucketToggleAction extends BasicBlockToggleAction // TODO: FML Flui
     @Override
     public ItemStack[] harvestBlock(World world, int x, int y, int z, EntityPlayer player, IToggleController controller)
     {
-        System.out.println("Harvesting");
-        ItemStack emptyBucket = controller.getStorageHandler().getItemFromStorage(new ItemStack(Items.bucket));
-        if (emptyBucket != null)
+        Block block = world.getBlock(x, y, z);
+        System.out.println(block.getClass().getSimpleName());
+        if (block instanceof IFluidBlock)
         {
-            Block block = world.getBlock(x, y, z);
-            if (block != null && (block == Blocks.water || block == Blocks.lava))
+            IToggleStorage storage = controller.getStorageHandler();
+            IFluidBlock fluidBlock = (IFluidBlock) block;
+            FluidStack containing = fluidBlock.drain(world, x, y, z, true);
+            ItemStack emptyContainer = null;
+            for (int s = 0; s < storage.getStorageSlots(); s++)
+            {
+                ItemStack inSlot = storage.getItemFromSlot(s);
+                if (FluidContainerRegistry.isEmptyContainer(inSlot))
+                {
+                    emptyContainer = inSlot;
+                    break;
+                }
+            }
+            if (emptyContainer == null) return null;
+            ItemStack filledContainer = FluidContainerRegistry.fillFluidContainer(containing, emptyContainer);
+            emptyContainer.stackSize--;
+            return new ItemStack[]{filledContainer};
+        } else if (block instanceof BlockStaticLiquid)
+        {
+            BlockStaticLiquid liquid = (BlockStaticLiquid) block;
+            ItemStack emptyBucket = controller.getStorageHandler().getItemFromStorage(new ItemStack(Items.bucket));
+            if (emptyBucket != null)
             {
                 emptyBucket.stackSize--;
-                ItemStack filledBucket;
-                if (block == Blocks.water)
-                    filledBucket = new ItemStack(Items.water_bucket);
-                else filledBucket = new ItemStack(Items.lava_bucket);
+                ItemStack filledBucket = null;
+                if (block == Blocks.water) filledBucket = new ItemStack(Items.water_bucket);
+                else if (block == Blocks.lava) filledBucket = new ItemStack(Items.lava_bucket);
                 world.setBlockToAir(x, y, z);
-                System.out.println("Returning");
-                return new ItemStack[]{filledBucket};
+                return filledBucket != null ? new ItemStack[]{filledBucket} : null;
             }
-            System.out.println("Block was null or not water/lava");
         }
-        System.out.println("Bucket stack was null");
         return null;
-        /*Block block = world.getBlock(x, y, z);
-        Fluid fromBlock = FluidRegistry.lookupFluidForBlock(block);
-        if (fromBlock == null)
-            return null;
-        ItemStack[] storage = controller.getAllStorage();
-        ItemStack emptyContainer = null;
-        for (ItemStack stack : storage)
-            if (stack != null)
-                if (FluidContainerRegistry.isEmptyContainer(stack))
-                    if (FluidContainerRegistry.fillFluidContainer(new FluidStack(fromBlock, 1000), stack) != null)
-                        emptyContainer = stack;
-        if (emptyContainer != null)
-        {
-            FluidStack fluidStack = new FluidStack(fromBlock, 1000);
-            ItemStack filledContainer = FluidContainerRegistry.fillFluidContainer(fluidStack, emptyContainer);
-            if (filledContainer != null)
-            {
-//                controller.removeItemFromStorage(emptyContainer);
-                emptyContainer.stackSize--;
-                world.setBlockToAir(x, y, z);
-                return new ItemStack[]{filledContainer};
-            }
-        }
-        return null;*/
     }
 
     @Override
     public void placeBlock(World world, int x, int y, int z, ForgeDirection direction, EntityPlayer player,
-                           ItemStack placing, IToggleController controller)
+                           ItemStack placing, IToggleController controller) // TODO: Add storage item index as parameter?
     {
-//        System.out.println("Placing block");
-//        placing.tryPlaceItemIntoWorld(player, world, x, y-1, z, ForgeDirection.UP.ordinal(), 0, 0, 0);
-//        if (FluidContainerRegistry.isFilledContainer(placing))
-//        {
-//            FluidStack fluidStack = FluidContainerRegistry.getFluidForFilledItem(placing);
-//            Fluid fluid = fluidStack.getFluid();
-//            if (fluidStack.getFluid().canBePlacedInWorld())
-//            {
-//                Block fluidBlock = fluid.getBlock();
-//                world.setBlock(x, y, z, fluidBlock);
-//                world.notifyBlockChange(x,y,z,fluidBlock);
-//                fluidBlock.onBlockAdded(world, x, y, z);
-//                ItemStack drainedContainer = FluidContainerRegistry.drainFluidContainer(placing);
-//                placing.stackSize--;
-//                controller.addItemStackToStorage(drainedContainer);
-//            }
-//        }
-
-        if (((ItemBucket) placing.getItem()).tryPlaceContainedLiquid(world, x, y, z))
+        if (FluidContainerRegistry.isFilledContainer(placing))
         {
-            placing.stackSize--;
-            controller.getStorageHandler().validateStorage();
-            controller.getStorageHandler().addItemStackToStorage(new ItemStack(Items.bucket));
+            FluidStack fluidStack = FluidContainerRegistry.getFluidForFilledItem(placing);
+            Fluid fluid = fluidStack.getFluid();
+            if (fluidStack.getFluid().canBePlacedInWorld())
+            {
+                Block fluidBlock = fluid.getBlock();
+                world.setBlock(x, y, z, fluidBlock);
+                world.notifyBlockChange(x, y, z, fluidBlock);
+                fluidBlock.onBlockAdded(world, x, y, z);
+                ItemStack drainedContainer = FluidContainerRegistry.drainFluidContainer(placing);
+                placing.stackSize--;
+                controller.getStorageHandler().addItemStackToStorage(drainedContainer);
+            }
+        } else if (placing.getItem() instanceof ItemBucket)
+        {
+            ItemBucket item = (ItemBucket) placing.getItem();
+            item.tryPlaceContainedLiquid(world, x, y, z);
         }
     }
 
     @Override
     public boolean canPlaceBlock(World world, int x, int y, int z, ItemStack placing, IToggleController controller)
     {
-//        return FluidContainerRegistry.isFilledContainer(placing);
-        return placing.getItem() instanceof ItemBucket && placing.getItem() != Items.bucket;
+        return FluidContainerRegistry.isFilledContainer(placing) || placing.getItem() instanceof ItemBucket;
+//        return placing.getItem() instanceof ItemBucket && placing.getItem() != Items.bucket;
     }
 
     @Override
     public boolean canHarvestBlock(World world, int x, int y, int z, IToggleController controller)
     {
         Block block = world.getBlock(x, y, z);
-        return block != null && (block == Blocks.water || block == Blocks.lava);
+        System.out.println(block.getClass().getSimpleName());
+        return (block instanceof IFluidBlock && ((IFluidBlock) block).getFilledPercentage(world, x, y, z) > 0) ||
+                block instanceof BlockStaticLiquid;
+
+//        Block block = world.getBlock(x, y, z);
+//        return block != null && (block == Blocks.water || block == Blocks.lava);
 
         /*Block block = world.getBlock(x, y, z);
         if (block == null)
