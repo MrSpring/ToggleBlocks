aef81755c568db7cd539fda15d0f4d45984bc6de
MrSpring <mrspring@mrspring.dk>
2015-09-29 19:43:05 +0200

    Made version be placed by gradle

--- src/main/java/dk/mrspring/toggle/ModInfo.java
+++ src/main/java/dk/mrspring/toggle/ModInfo.java
@@ -7,5 +7,5 @@ public class ModInfo
 {
     public static final String MOD_ID = "tb";
     public static final String NAME = "Toggle Blocks";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "${version}";
 }
